4ad6c5ffd41c224ca7d69210a5656158d2112f7d
Sameer Fidai <sameerfidai@Sameers-MBP.attlocal.net>
2021-08-10 14:28:30 -0400

    Added cart page (also added cart counter on cart icon), added add to cart button on main page, and added firebase to the project.

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDGELV5xaSBGVP553leYhcsCSg6pDX2thU",
+    authDomain: "clone-two-93a79.firebaseapp.com",
+    projectId: "clone-two-93a79",
+    storageBucket: "clone-two-93a79.appspot.com",
+    messagingSenderId: "214484844456",
+    appId: "1:214484844456:web:160f095d1a76b0e7b2e79d",
+    measurementId: "G-DZ5CJHTY3L"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
